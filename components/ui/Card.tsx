3bf771f397e6ff91dca48f6fc7e05fa6887d088f
Thanyatab chumponrob <106069231+captainthx@users.noreply.github.com>
2025-01-07 22:26:39 +0700

    fix: card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,28 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { ThemedView } from "../ThemedView";
 
 interface CardProps extends React.PropsWithChildren {
-  lightColor?: string;
-  darkColor?: string;
-  name: String;
+  style?: Object;
 }
 
-export default function Card({ children }: CardProps) {
+export default function Card({ children, style }: CardProps) {
   return (
-    <View
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-        marginTop: 24,
-        marginHorizontal: 20,
-        height: 150,
-        borderRadius: 16,
-      }}
+    <ThemedView
+      style={[
+        {
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 24,
+          marginHorizontal: 20,
+          height: 150,
+          borderRadius: 16,
+        },
+        style,
+      ]}
     >
       {children}
-    </View>
+    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({});
